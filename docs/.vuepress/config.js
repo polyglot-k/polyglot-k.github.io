7753fa92d2d4e31c8e9c6e0a478d5bfecaea0cbb
KNU-K <knukang334@gmail.com>
2025-09-27 17:43:35 +0900

    기본 경로와 블로그 제목 및 설명을 업데이트

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,7 +1,7 @@
 module.exports = {
-    base: "/polyglot-k.github.io/",
-    title: "나의 블로그",
-    description: "VuePress로 만든 나의 멋진 블로그",
+    base: "/",
+    title: "Polyglot-K의 블로그",
+    description: "다양한 도전을 즐기는 개발자 Polyglot-K의 블로그입니다.",
     head: [["link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" }]],
     themeConfig: {
         logo: "https://avatars.githubusercontent.com/u/126179088?v=4",
@@ -15,7 +15,7 @@ module.exports = {
                 {
                     title: "게시물 목록",
                     collapsable: false,
-                    children: ["2025-09-27-first-post.md", "2025-09-28-second-post.md"],
+                    children: ["2025-09-27-spring-webflux-performance.md"],
                 },
             ],
         },
