99801ced2aefddb5c60f49ee33a8334d170cf34e
KNU-K <knukang334@gmail.com>
2025-09-28 19:28:26 +0900

    설정 파일 추가 및 게시물 업데이트: Giscus 설정 추가, 게시물 링크 및 목록 수정

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -8,14 +8,14 @@ module.exports = {
         sidebarDepth: 2,
         nav: [
             { text: "Home", link: "/" },
-            { text: "Posts", link: "/posts/2025-09-27-spring-webflux-performance.html" },
+            { text: "Posts", link: "/posts/2025-08-25-spring-webflux-performance.html" },
         ],
         sidebar: {
             "/posts/": [
                 {
                     title: "게시물 목록",
                     collapsable: false,
-                    children: ["2025-09-27-spring-webflux-performance.md"],
+                    children: ["2025-07-15-text-based-real-time-streaming-virtual-interview.md", "2025-07-20-ice-breaking-platform.md", "2025-08-05-valkey-concurrency-issue.md", "2025-08-10-scheduler-mq-data-archiving.md", "2025-08-25-spring-webflux-performance.md"],
                 },
             ],
         },
