879bdd3a325dace6c3e303965c996e4a2bb95f16
KNU-K <knukang334@gmail.com>
2025-09-30 19:32:07 +0900

    로고 URL 제거: config.js에서 불필요한 로고 URL 삭제

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -23,7 +23,6 @@ module.exports = {
     description: "다양한 도전을 즐기는 개발자 Polyglot-K의 블로그입니다.",
     head: [["link", { rel: "stylesheet", href: "https://cdn.jsdelivr.net/gh/orioncactus/pretendard@v1.3.9/dist/web/static/pretendard.min.css" }]],
     themeConfig: {
-        logo: "https://avatars.githubusercontent.com/u/126179088?v=4",
         sidebarDepth: 2,
         nav: [
             { text: "Home", link: "/" },
